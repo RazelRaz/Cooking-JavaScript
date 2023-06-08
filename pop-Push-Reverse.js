fd16b88ff9dee7da7d86a35be5bbb795aa6b144a
RazelRaz <razel16dcm@gmail.com>
2023-06-08 22:48:37 +0600

    Array Pop Push Reverse

---
+++ pop-Push-Reverse.js
@@ -0,0 +1,17 @@
+// array reverse
+const arr1 = ['a',"b","c"];
+const res1 = arr1.reverse()
+console.log(res1);
+
+//array push - add in the last
+const arr2 = [22,56,89];
+const res2 = arr2.push(91);
+console.log(res2);
+console.log(arr2);
+
+//array pop - remove last element of an array, and return removed element
+
+const arr3 = [1,2,3,4,5,6];
+const res3 = arr3.pop()
+console.log(res3);
+console.log(arr3);
