ad63c84b29c9dde60e5e96c155503ced8a2b33f8
RazelRaz <razel16dcm@gmail.com>
2023-06-08 23:02:39 +0600

    Array Slice And Sort

---
+++ slice-sort.js
@@ -0,0 +1,9 @@
+// array sort & slice
+
+const numbers = [2,13,19,43,56,67,88];
+const res = numbers.sort()
+console.log(res);
+
+const numbers2 = [2,13,19,43,56,67,88];
+const res2 = numbers2.slice(2,4)
+console.log(res2);
