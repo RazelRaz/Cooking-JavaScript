e72bbebf5817cad59ecd003f9a9a6e8510ab01a4
RazelRaz <razel16dcm@gmail.com>
2023-06-08 21:57:09 +0600

    Array Concate And Array Form

---
+++ array-concat-array-form-.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'green', 'black'];
+const fruits = ['apple', 'berry'];
+
+const add = colors.concat(fruits)
+console.log(add);
+
+
+const content = "Quick brown fox."
+const maleAnArray = Array.from(content);
+console.log(maleAnArray[9]);
