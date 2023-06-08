76891f88987a0c1c401d27c49520eb15b9008971
RazelRaz <razel16dcm@gmail.com>
2023-06-08 22:34:48 +0600

    Array Includes And IndexOf

---
+++ includes-indexOF.js
@@ -0,0 +1,6 @@
+// Array Includes And IndexOf
+const namelist = ['John', 'Antony', 'Gibbs', "Ashley"];
+const result = namelist.includes("Gibbs");
+console.log(result);
+const result2 = namelist.indexOf("Ashley")
+console.log(result2);
