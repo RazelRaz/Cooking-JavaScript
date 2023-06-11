f6e9a8dfa19190a3a83c8ab3fe0610a2c70e6fc6
RazelRaz <razel16dcm@gmail.com>
2023-06-11 15:18:01 +0600

    array reduce

---
+++ reduce.js
@@ -0,0 +1,8 @@
+var numbers = [1,2,3,4,5,6];
+
+var sum = numbers.reduce((prevValue, currentValue) => {
+    return prevValue + currentValue;
+}, 0)
+
+
+console.log(sum);
---
+++ string-Methods.js
@@ -0,0 +1,7 @@
+const doc = "Quick brown fox";
+const res =  doc.charAt(3);
+console.log(res);
+
+const doc2 = " jumps over the lazy dog."
+const res2 = doc.concat(doc2)
+console.log(res2);
