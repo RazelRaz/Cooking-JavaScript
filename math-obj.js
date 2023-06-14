4ea133df1628d0cddf1e7c2ff39b263083d6189d
RazelRaz <razel16dcm@gmail.com>
2023-06-14 20:27:55 +0600

    math object

---
+++ math-obj.js
@@ -0,0 +1,8 @@
+var num = -7;
+var num2 = 7.3;
+var num3 = 9.9
+console.log(Math.abs(num));
+console.log(Math.ceil(num2));
+console.log(Math.floor(num3));
+console.log(Math.max(22,34,56));
+console.log(Math.min(22,34,56));
