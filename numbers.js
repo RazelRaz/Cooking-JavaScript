9ccd47adfa20e4020ffc52ab89253e672723f335
RazelRaz <razel16dcm@gmail.com>
2023-06-14 20:38:04 +0600

    javascript numbers

---
+++ numbers.js
@@ -0,0 +1,8 @@
+const num = 5.6;
+const num2 = "4.567";
+const num3 = 4.56345667;
+console.log(Number.isInteger(num));
+console.log(Number.isFinite(num));
+console.log(Number.parseFloat(num2));
+console.log(Number.parseInt(num2));
+console.log(num3.toFixed(4));
