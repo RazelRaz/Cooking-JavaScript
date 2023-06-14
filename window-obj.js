e52f310962c04ed6f3735c2c3e23acc6fa74b604
RazelRaz <razel16dcm@gmail.com>
2023-06-14 22:55:29 +0600

    window object

---
+++ window-obj.js
@@ -0,0 +1,23 @@
+// window object method
+
+function wObj() {
+    alert('Error 404')
+}
+
+function wConfirm(){
+   const result = confirm('Do you want confirm')
+   document.write(result)
+}
+
+function wPrompt(){
+    const result = prompt('Type Your Name');
+    document.write(result)
+}
+
+function wOpen(){
+    open();
+}
+
+function wCLose(){
+    close();
+}
