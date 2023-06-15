8703efa9667354e072922a053f1568591296851d
RazelRaz <razel16dcm@gmail.com>
2023-06-15 20:01:25 +0600

    dome manipulation

---
+++ dom/script.js
@@ -0,0 +1,7 @@
+const head =  document.getElementById('myId').innerHTML = 'Hello';
+
+var myCls = document.getElementsByClassName('myCls');
+myCls[0].innerHTML = 'Hello Para';
+
+var six = document.getElementsByTagName('h6')
+six[0].innerHTML = 'This is Heading'
